50dd08c4ead520bd6c7ad817834dd6addb924c47
Riccardo Degan <riccardo.degan@diginess.it>
2012-07-23 09:45:16 +0200

    Fix respond with

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,10 +1,8 @@
 Spree::OrdersController.class_eval do
    def print_order
        @order = Spree::Order.find_by_number!(params[:order_number])
-       respond_with(@order, :status => :ok) do |format|
-         format.html {
-           render :action => "print_order" ,:layout => false
-         }
+       respond_with(@order, :status => :ok,:layout => false) do |format|
+         format.html 
        end
    end
 end
